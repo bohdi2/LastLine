adae8fba98156015851970592902c70bc20c34fc
Christopher Helck <christopher.helck@gmail.com>
2015-04-09 10:37:09 -0400

    refactor

--- src/main/java/com/example/impl/ForwardFileChunker.java
+++ src/main/java/com/example/impl/ForwardFileChunker.java
@@ -1,6 +1,7 @@
 package com.example.impl;
 
 import com.example.FileIterator;
+
 import java.io.File;
 import java.io.IOException;
 import java.io.RandomAccessFile;
@@ -8,64 +9,60 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.NoSuchElementException;
 
-public class ForwardFileChunker  {
+public class ForwardFileChunker implements FileIterator<List<Long>> {
+    private final int CHUNK_SIZE = 1024;
+
+    private final RandomAccessFile m_file;
+    private final byte[] m_buffer;
 
     public static FileIterator create(File file) throws IOException {
-        return new ChunkIter(file);
+        return new ForwardFileChunker(file);
     }
 
-    static class ChunkIter implements FileIterator<List<Long>> {
-        private final int CHUNK_SIZE = 1024;
 
-        private final RandomAccessFile m_file;
-        private final byte[] m_buffer;
+    public ForwardFileChunker(File file) throws IOException {
+        m_file = new RandomAccessFile(file, "r");
+        m_buffer = new byte[CHUNK_SIZE];
+    }
 
-        public ChunkIter(File file) throws IOException {
-            m_file = new RandomAccessFile(file, "r");
-            m_buffer = new byte[CHUNK_SIZE];
+    public boolean hasNext() {
+        try {
+            return nextChunkSize() > 0;
+        } catch (IOException e) {
+            return false;
+        }
+    }
+
+    public List<Long> next() {
+
+        try {
+            long position = m_file.getFilePointer();
+            int size = nextChunkSize();
+
+            m_file.readFully(m_buffer, 0, size);
+            return Util.getOffsets(position, m_buffer, size);
+        } catch (IOException e) {
+            throw new NoSuchElementException();
         }
 
-        public boolean hasNext() {
-            try {
-                return nextChunkSize() > 0;
-            }
-            catch (IOException e) {
-                return false;
-            }
-        }
+    }
 
-        public List<Long> next() {
+    public void remove() {
+        throw new UnsupportedOperationException("remove not supported");
+    }
 
-            try {
-                long position = m_file.getFilePointer();
-                int size = nextChunkSize();
+    public void close() throws IOException {
+        m_file.close();
+    }
 
-                m_file.readFully(m_buffer, 0, size);
-                return Util.getOffsets(position, m_buffer, size);
-            }
-            catch (IOException e) {
-                throw new NoSuchElementException();
-            }
+    private int nextChunkSize() throws IOException {
+        long remaining = m_file.length() - m_file.getFilePointer();
 
-        }
-
-        public void remove() {
-            throw new UnsupportedOperationException("remove not supported");
-        }
-
-        public void close() throws IOException {
-            m_file.close();
-        }
-
-        private int nextChunkSize() throws IOException {
-            long remaining = m_file.length() - m_file.getFilePointer();
-
-            if (remaining > CHUNK_SIZE)
-                return CHUNK_SIZE;
-            else
-                return (int) remaining;
-        }
-
+        if (remaining > CHUNK_SIZE)
+            return CHUNK_SIZE;
+        else
+            return (int) remaining;
     }
 
 }
+
--- src/main/java/com/example/impl/ReverseFileChunker.java
+++ src/main/java/com/example/impl/ReverseFileChunker.java
@@ -9,74 +9,71 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.NoSuchElementException;
 
-public class ReverseFileChunker  {
+public class ReverseFileChunker implements FileIterator<List<Long>> {
+    private final int CHUNK_SIZE = 1024;
+
+    private final RandomAccessFile m_file;
+    private final byte[] m_buffer;
 
     public static FileIterator create(File file) throws IOException {
-        return new ReverseChunkIter(file);
+        return new ReverseFileChunker(file);
     }
 
-    static class ReverseChunkIter implements FileIterator<List<Long>> {
-        private final int CHUNK_SIZE = 1024;
 
-        private final RandomAccessFile m_file;
-        private final byte[] m_buffer;
+    public ReverseFileChunker(File file) throws IOException {
+        m_file = new RandomAccessFile(file, "r");
+        m_file.seek(m_file.length());
+        m_buffer = new byte[CHUNK_SIZE];
+    }
 
-        public ReverseChunkIter(File file) throws IOException {
-            m_file = new RandomAccessFile(file, "r");
-            m_file.seek(m_file.length());
-            m_buffer = new byte[CHUNK_SIZE];
+    public boolean hasNext() {
+        try {
+            //System.err.format("ReverseFileChunker.hasNext() %d%n", nextChunkSize());
+            return nextChunkSize() > 0;
+        } catch (IOException e) {
+            return false;
+        }
+    }
+
+    public List<Long> next() {
+
+        try {
+            long position = m_file.getFilePointer();
+            int size = nextChunkSize();
+
+            //System.err.format("ReverseFileChunker.next(): position: %d, size: %d%n",
+            //                  position,
+            //                  size);
+
+            m_file.seek(position - size);
+            m_file.readFully(m_buffer, 0, size);
+            m_file.seek(position - size);
+
+            //System.err.format("ReverseFileChunker.next().exit: %d%n", m_file.getFilePointer());
+            return Util.getReverseOffsets(m_file.length(), position - size, m_buffer, size);
+        } catch (IOException e) {
+            throw new NoSuchElementException();
         }
 
-        public boolean hasNext() {
-            try {
-                //System.err.format("ReverseFileChunker.hasNext() %d%n", nextChunkSize());
-                return nextChunkSize() > 0;
-            }
-            catch (IOException e) {
-                return false;
-            }
-        }
+    }
 
-        public List<Long> next() {
+    public void remove() {
+        throw new UnsupportedOperationException("remove not supported");
+    }
 
-            try {
-                long position = m_file.getFilePointer();
-                int size = nextChunkSize();
+    public void close() throws IOException {
+        m_file.close();
+    }
 
-                //System.err.format("ReverseFileChunker.next(): position: %d, size: %d%n",
-                //                  position,
-                //                  size);
+    private int nextChunkSize() throws IOException {
+        long remaining = m_file.getFilePointer();
 
-                m_file.seek(position - size);
-                m_file.readFully(m_buffer, 0, size);
-                m_file.seek(position - size);
-
-                //System.err.format("ReverseFileChunker.next().exit: %d%n", m_file.getFilePointer());
-                return Util.getReverseOffsets(m_file.length(), position-size, m_buffer, size);
-            }
-            catch (IOException e) {
-                throw new NoSuchElementException();
-            }
-
-        }
-
-        public void remove() {
-            throw new UnsupportedOperationException("remove not supported");
-        }
-
-        public void close() throws IOException {
-            m_file.close();
-        }
-
-        private int nextChunkSize() throws IOException {
-            long remaining = m_file.getFilePointer();
-
-            if (remaining > CHUNK_SIZE)
-                return CHUNK_SIZE;
-            else
-                return (int) remaining;
-        }
-
+        if (remaining > CHUNK_SIZE)
+            return CHUNK_SIZE;
+        else
+            return (int) remaining;
     }
 
 }
+
+
